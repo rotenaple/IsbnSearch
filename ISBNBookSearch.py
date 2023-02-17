a3deed6a796456c494695c5aa674de393e11e481
rotenaple <rotenaple@protonmail.com>
2023-02-17 14:27:03 +1030

    /

--- ISBNBookSearch.py
+++ ISBNBookSearch.py
@@ -1,5 +1,9 @@
 import requests
 import xml.dom.minidom
+import tkinter as tk
+import re
+import webbrowser
+
 
 def convert_isbn10_to_isbn13(isbn):
     if len(isbn) == 10:
@@ -11,11 +15,12 @@ def convert_isbn10_to_isbn13(isbn):
         isbn = isbn13_without_check + str(check_digit)
     return isbn
 
+
 def get_info(isbn):
     title, author, publisher, publication_year, nsfa = None, None, None, None, None
 
     # Query OCLC API for book information
-    url = f"http://classify.oclc.org/classify2/Classify?isbn={isbn}"
+    url = f"http://classify.oclc.org/classify2/Classify?isbn={isbn}&summary=true"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -40,7 +45,6 @@ def get_info(isbn):
     if response.status_code == 200:
         data = response.json()
         if data["totalItems"] > 0:
-
             book_info = data["items"][0]["volumeInfo"]
             title = book_info.get("title")
             authors = book_info.get("authors")
@@ -48,7 +52,8 @@ def get_info(isbn):
             publisher = book_info.get("publisher")
             publication_year = book_info.get("publishedDate")
 
-    return (title, author, publisher, publication_year, nsfa)
+    return title, author, publisher, publication_year, nsfa
+
 
 def get_price(isbn):
     url = "https://www.abebooks.com/servlet/DWRestService/pricingservice"
@@ -60,17 +65,13 @@ def get_price(isbn):
     if results['success']:
         best_new = results['pricingInfoForBestNew']
         best_used = results['pricingInfoForBestUsed']
-        return ((best_new['bestPriceInPurchaseCurrencyWithCurrencySymbol']),(best_used['bestPriceInPurchaseCurrencyWithCurrencySymbol']))
-    return (None, None)
+        return ((best_new['bestPriceInPurchaseCurrencyWithCurrencySymbol']),
+                (best_used['bestPriceInPurchaseCurrencyWithCurrencySymbol']))
+    return [None, None]
 
-
-# Basic UI
-import tkinter as tk
-import re
-import webbrowser
 
 def search():
-    isbn = entry.get().strip().replace('-', '').replace(' ', '')
+    entry.get().strip().replace('-', '').replace(' ', '')
     isbn = re.sub(r'\D', '', entry.get())
 
     if len(isbn) not in [10, 13]:
@@ -85,10 +86,10 @@ def search():
 
     title, author, publisher, publication_year, ddc = get_info(isbn)
     best_new, best_used = get_price(isbn)
-    
+
     # Initialize count variable
     count_hidden = 0
-    
+
     # Build result string, hiding empty fields
     result_text = ""
     if title:
@@ -120,7 +121,7 @@ def search():
         result_text += f"Used Book Price: {best_used}\n"
     else:
         count_hidden += 1
-    
+
         # Show message if any data is hidden
     if count_hidden == 1:
         result_text += f"({count_hidden} empty data entry is hidden)"
@@ -135,57 +136,73 @@ def search():
     # Show the result text
     result.configure(text=result_text)
 
+
 def open_abebooks():
-    isbn = entry.get().strip().replace('-', '').replace(' ', '')
+    entry.get().strip().replace('-', '').replace(' ', '')
     isbn = re.sub(r'\D', '', entry.get())
     webbrowser.open(f'https://www.abebooks.com/servlet/SearchResults?kn={isbn}')
 
+
 def open_bookfinder():
-    isbn = entry.get().strip().replace('-', '').replace(' ', '')
+    entry.get().strip().replace('-', '').replace(' ', '')
     isbn = re.sub(r'\D', '', entry.get())
-    webbrowser.open(f'https://www.bookfinder.com/isbn/{isbn}/')  
-
-root = tk.Tk()
-root.title("Book Search")
-root.wm_attributes('-topmost', 1) # always on top
-
-label = tk.Label(root, text="Enter an ISBN-10 or ISBN-13 code:")
-label.pack()
-
-entry = tk.Entry(root)
-entry.pack()
-
-button = tk.Button(root, text="Search", command=search)
-button.pack()
-
-result = tk.Label(root, text="")
-result.pack()
-
-link1 = tk.Button(root, text="Find on Abebooks", command=open_abebooks, state='normal')
-link2 = tk.Button(root, text="Find on Bookfinder", command=open_bookfinder, state='normal')
+    webbrowser.open(f'https://www.bookfinder.com/isbn/{isbn}/')
 
 
+ui_mode = False
+if ui_mode:
+    root = tk.Tk()
+    root.title("Book Search")
+    root.wm_attributes('-topmost', 1)  # always on top
 
-root.mainloop()
+    label = tk.Label(root, text="Enter an ISBN-10 or ISBN-13 code:")
+    label.pack()
 
-"""
-# Terminal Only
-def main():
-    isbn = input("Enter an ISBN-10 or ISBN-13 code: ")
-    isbn = isbn.replace('-', '').replace(' ', '')
-    if len(isbn) == 10:
-        isbn = convert_isbn10_to_isbn13(isbn)
+    entry = tk.Entry(root)
+    entry.pack()
 
-    title, author, publisher, publication_year = get_info_Google(isbn)
-    best_new, best_used = get_info_Abebooks(isbn)
+    button = tk.Button(root, text="Search", command=search)
+    button.pack()
 
-    print(f"Title: {title}")
-    print(f"Author(s): {author}")
-    print(f"Publisher: {publisher}")
-    print(f"Publication Year: {publication_year}")
-    print(f"https://www.abebooks.com/servlet/SearchResults?kn={isbn}")
-    print(f"New Book Price: {best_new}")
-    print(f"Used Book Price: {best_used}")
+    result = tk.Label(root, text="")
+    result.pack()
 
-main()
-"""
+    link1 = tk.Button(root, text="Find on Abebooks", command=open_abebooks, state='normal')
+    link2 = tk.Button(root, text="Find on Bookfinder", command=open_bookfinder, state='normal')
+
+    root.mainloop()
+
+else:
+
+    def main():
+        while True:
+            error = False
+            isbn = input("Enter an ISBN-10 or ISBN-13 code: ")
+            isbn = isbn.replace('-', '').replace(' ', '')
+
+            if len(isbn) not in [10, 13]:
+                print(f"Error: ISBN must be 10 or 13 digits")
+                error = True
+
+            if len(isbn) == 10:
+                isbn = convert_isbn10_to_isbn13(isbn)
+
+            title, author, publisher, publication_year, ddc = get_info(isbn)
+            best_new, best_used = get_price(isbn)
+
+            if not error:
+                print(f"\n")
+                print(f"Title: {title}")
+                print(f"Author(s): {author}")
+                print(f"ISBN-13: {isbn}")
+                print(f"DDC: {ddc}")
+                print(f"Publisher: {publisher}")
+                print(f"Publication Year: {publication_year}")
+                print(f"Open in Abebooks: https://www.abebooks.com/servlet/SearchResults?kn={isbn}")
+                print(f"Open in Bookfinder: https://www.bookfinder.com/isbn/{isbn}")
+                print(f"New Book Price: {best_new}")
+                print(f"Used Book Price: {best_used}")
+
+
+
+    main()
